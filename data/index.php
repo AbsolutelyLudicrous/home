d9a2303e24fd5d319b3df538d15d1c8a50c60960
Danne <mdanne@protonmail.com>
2018-02-23 22:16:54 -0500

    Added a webpage title

--- data/index.php
+++ data/index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
+	<title>Eostre Danne's personal website</title>
 	<body>
 		<p>
 			Hey hey hey y'all.
 			Welcome to the personal website of Eostre Danne.
 			I stick stuff here.
 		</p>
-		<?php include('index-footer.php'); ?>
 	</body>
+	<?php include('index-footer.php'); ?>
 </html>
