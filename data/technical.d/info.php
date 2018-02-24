fab8721a6f79fbc8f42d2bc66ae564d02156fb56
Danne <mdanne@protonmail.com>
2018-02-23 21:39:50 -0500

    Made my PHP less Pythony

--- data/technical.d/info.php
+++ data/technical.d/info.php
@@ -1,6 +1,7 @@
 <?php 
 	if(file_exists('/usr/local/www/apache24/lockfiles/WEBSERVER_DEBUGGING')){
-		phpinfo();}
-	else{
-		echo('The webserver does not currently have debugging information turned on, email mdanne@protonmail.com if you would like technical information');}
+		phpinfo();
+	}else{
+		echo('The webserver currently has this function disabled.');
+	}
 ?>
