30633dc78380236941dc845706036baba62a8155
Eostre <mdanne@protonmail.com>
2018-02-23 23:58:28 -0500

    Added index files for explorers.d

---
+++ data/explorers.d/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<p>
+			This directory contains materials for the Explorer's Post 891 computer science and nerdery group.
+			<a href="http://post891.org">Their website</a> is currently (as of 20180223 2356) not open.
+			<br>
+			Post 891 is where a bunch of nerds get together and learn about Unix, programming, networking, hardware, or really anything you'd need in industry.
+		</p>
+	</body>
+	<?php include('index-footer.php'); ?>
+</html>
