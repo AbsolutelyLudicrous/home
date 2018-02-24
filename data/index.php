f38cf1d44156a58d9708f329931ef841e0c3be09
Eostre Danne <mdanne@protonmail.com>
2018-02-24 15:05:54 -0500

    Added a wetty hyperlink

--- data/index.php
+++ data/index.php
@@ -5,7 +5,9 @@
 		<p>
 			Hey hey hey y'all.
 			Welcome to the personal website of Eostre Danne.
-			I stick stuff here.
+			I stick stuff here sometimes.
+			<br>
+			<a href="http://danne.site:3447/wetty/">wetty access</a>
 		</p>
 	</body>
 	<?php include('index-footer.php'); ?>
