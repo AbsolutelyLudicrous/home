e1c344ba22e5a4305764c396e5c5cbc2d14eb528
Spiderman <webadmin@danne.site>
2018-02-25 07:50:30 -0800

    I guess I have a blog now? Neat.

---
+++ data/blag.d/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<title>Eostre Danne's personal blog</title>
+	<body>
+		<p>
+			I dunno, I guess I have a blog now?
+			Wild.
+			I was kinda using Mastodon for blogging purposes,
+			but I'll probably stick longer stuff up here,
+			since Mastodon has got a 500char size limit.
+			<br>
+			So yeah, I dump my thoughts here.
+			Read 'em if you want.
+		</p>
+	</body>
+	<?php include('index-footer.php'); ?>
+</html>
