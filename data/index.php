045b301212ee22f3e30e48d94b9862b0711a1ed2
Spiderman <webadmin@danne.site>
2018-05-14 17:01:34 -0700

    Threw the blog under here

--- data/index.php
+++ data/index.php
@@ -6,8 +6,6 @@
 			Hey hey hey y'all.
 			Welcome to the personal website of Eostre Danne.
 			I stick stuff here sometimes.
-			<br>
-			<a href="http://danne.site:3447/wetty/">wetty access</a>
 		</p>
 	</body>
 	<?php include('index-footer.php'); ?>
